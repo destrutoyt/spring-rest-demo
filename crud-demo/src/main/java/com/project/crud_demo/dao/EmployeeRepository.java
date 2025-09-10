466c13494e273012391838f2f09eb2472bca05dc
Miguel Garces <miguelongriego24@gmail.com>
2025-09-09 20:18:05 -0400

    Implement OpenAPI and Swagger UI for API documentation.

--- crud-demo/src/main/java/com/project/crud_demo/dao/EmployeeRepository.java
+++ crud-demo/src/main/java/com/project/crud_demo/dao/EmployeeRepository.java
@@ -1,9 +1,11 @@
 package com.project.crud_demo.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import com.project.crud_demo.entity.Employee;
 
+@RepositoryRestResource(path="members") // http://localhost:8080/members instead of http://localhost:8080/employees
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 
 }
